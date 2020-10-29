7f94edb225308532c5764012374db0feb161a8f3
815129099 <815129099@qq.com>
2020-10-29 18:35:14 +0800

    the four 第二步完成

--- src/app/pages/passport/signup/signup.page.ts
+++ src/app/pages/passport/signup/signup.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { IonSlides } from '@ionic/angular';
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
 import { AuthenticationCodeService } from '../authentication-code.service';
 import {Signup} from './signup';
 
@@ -23,9 +24,13 @@ export class SignupPage implements OnInit {
   };
   phoneSubmited=false;
   codeSubmited=false;
+  //按钮切换
+  showButtonText = "发送验证码";
+  countDowmTime = 60;
+  countDown= false;
 
   slideIndex = 0;
-  constructor(private authenticationCodeService:AuthenticationCodeService) { }
+  constructor(private authenticationCodeService:AuthenticationCodeService,private localStorageService:LocalStorageService) { }
 
   ngOnInit() {
   //  this.signupSlides.lockSwipeToNext(true);
@@ -67,7 +72,30 @@ onSubmitCode(form:NgForm){
 }
 
 onSendSMS(){
+  let codeTime = this.localStorageService.get(this.signup.phone, 0);
+    if (codeTime == 3) {
+      this.showButtonText = '已获取三次';
+      this.countDown = true;
+      return;
+    } else {
+      codeTime++;
+      this.localStorageService.set(this.signup.phone, codeTime);
+    }
+
+
   this.authenticationCodeService.createCode();
+  this.countDown = true;                // 发送验证码后一分钟内，按钮变成不可点击状态 
+  this.showButtonText = '验证码已发送（' +60+ 's）';           // 验证码发送后的初始状态 
+  const start = setInterval(()=>{
+     if(this.countDowmTime>=0){
+       this.showButtonText = '验证码已发送('+this.countDowmTime--+'s)';
+     }else{
+       clearInterval(start);
+       this.countDowmTime = 60;
+       this.showButtonText = '重新发送';
+       this.countDown = false;
+     }
+  },1000)
 }
 
 onValidateCode(code:string):boolean{
